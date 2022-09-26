4b65c6f1931a08d082379f30b59683c8f5f2cfec
Titusz Pan <titusz.pan@gmail.com>
2022-09-26 12:12:42 +0200

    Document CLEANUP_INTERVAL setting.

--- iscc_web/options.py
+++ iscc_web/options.py
@@ -24,7 +24,9 @@ class IsccWebOptions(BaseSettings):
     storage_expiry: int = Field(
         3600, description="Number of seconds after which uploaded files are deleted"
     )
-    cleanup_interval: int = Field(600, description="Interval in seconds for running file cleanup")
+    cleanup_interval: int = Field(
+        600, description="Interval in seconds for running file cleanup. Use 0 to deactivate"
+    )
     log_level: str = Field("DEBUG", description="Set logging level")
 
 

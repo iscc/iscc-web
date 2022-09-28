8e43e00a8d6dff1aa4357f1887fb7e3f2f08a3ea
Titusz Pan <titusz.pan@gmail.com>
2022-09-28 21:17:38 +0200

    Tweak logging.

--- iscc_web/cleanup.py
+++ iscc_web/cleanup.py
@@ -8,13 +8,13 @@ from aiofiles.os import scandir, path
 
 async def cleanup_task():
     while True:
-        log.debug("Running cleanup session ...")
+        log.info("Running cleanup session ...", enqueue=True)
         for fp in await scandir(iw.opts.media_path):
             if fp.name in ["061knt35ejv6o", ".gitignore"]:
                 continue
             dir_time = int(await path.getctime(fp.path))
             cur_time = int(time.time())
             if cur_time > (dir_time + iw.opts.storage_expiry):
-                log.debug(f"Deleting expired {fp.name}")
+                log.info(f"Deleting expired {fp.name}", enqueue=True)
                 await iw.rmtree(fp.path)
         await asyncio.sleep(iw.opts.cleanup_interval)
